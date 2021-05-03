b249f79ba3212a2da3d94947b023799f7bd6cb21
Lakshman.S <haranhari_38@outlook.com>
2021-05-03 21:53:28 +0530

    Remove Public pages.

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -3,7 +3,7 @@ import winston from 'winston';
 import winstonDaily from 'winston-daily-rotate-file';
 
 // logs dir
-const logDir = __dirname + '/../logs';
+const logDir = process.cwd() + '/logs';
 
 if (!fs.existsSync(logDir)) {
   fs.mkdirSync(logDir);
